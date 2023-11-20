4c5f7807d60acc9345529398fba1be80090eef98
taira <100938045+tairasoul@users.noreply.github.com>
2023-11-20 13:24:32 +0200

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,57 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using System.Reflection;
+
+[assembly: MelonInfo(typeof(BuildRestrictionRemover.RestrictionRemover), "LC.BuildRestrictionRemover", "1.0.0", "tairasoul")]
+[assembly: MelonGame("ZeekerssRBLX", "Lethal Company")]
+
+namespace BuildRestrictionRemover
+{
+
+    [HarmonyPatch(typeof(ShipBuildModeManager))]
+    internal class BuildPatches
+    {
+        [HarmonyPatch("PlayerMeetsConditionsToBuild")]
+        [HarmonyPostfix]
+        static void PlayerConditions(ref bool __result)
+        {
+            __result = true;
+        }
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static void Update(ShipBuildModeManager __instance)
+        {
+            FieldInfo placingObject = typeof(ShipBuildModeManager).GetField("placingObject", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (placingObject != null)
+            {
+                PlaceableShipObject shipObject = (PlaceableShipObject)placingObject.GetValue(__instance);
+                if (shipObject != null)
+                {
+                    shipObject.AllowPlacementOnWalls = true;
+                    shipObject.AllowPlacementOnCounters = true;
+                    shipObject.doCollisionPointCheck = false;
+                }
+            }
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void UpdatePostfix(ShipBuildModeManager __instance)
+        {
+            FieldInfo CanConfirmPosition = typeof(ShipBuildModeManager).GetField("CanConfirmPosition", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (CanConfirmPosition != null)
+            {
+                CanConfirmPosition.SetValue(__instance, true);
+            }
+            __instance.ghostObjectRenderer.sharedMaterial = __instance.ghostObjectGreen;
+        }
+    }
+    public class RestrictionRemover: MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            HarmonyInstance.PatchAll(typeof(BuildPatches));
+            LoggerInstance.Msg("Applied ShipBuildModeManager patches.");
+        }
+    }
+}
